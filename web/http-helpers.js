87c590d07aa58244758d8581c5c5c74dba629b61
acrobat130 <shanna.sullivan@gmail.com>
2015-10-28 20:11:40 -0700

    can now grab content from archive

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -9,6 +9,16 @@ exports.headers = {
   "access-control-max-age": 10, // Seconds.
   'Content-Type': "text/html"
 };
+var archivedSitesArray = []
+fs.readdir(archive.paths.archivedSites, function(error, files){
+  // console.log("files---------------------", files);
+  files.forEach(function(file){
+    console.log(file)
+    archivedSitesArray.push(file)
+  })
+});
+
+
 
 exports.serveAssets = function(res, asset, callback) { // asset === website
   // Write some code here that helps serve up your static files!
@@ -18,6 +28,8 @@ exports.serveAssets = function(res, asset, callback) { // asset === website
   
 exports.sendResponse = function(req, res, statusCode){
   statusCode = statusCode || 200;
+    // console.log('req.url.substr(1) --------------->', archivedSitesArray.indexOf(req.url.substr(1)))
+    // console.log('archivedSitesArray -------->', archivedSitesArray)
   if (req.url === '/') {
     // console.log('archive.paths.css-------------------', archive.paths.css)
     fs.readFile(archive.paths.html, function(error, html) {
@@ -28,9 +40,11 @@ exports.sendResponse = function(req, res, statusCode){
       // console.log("res----------------", res);
       res.end(html);
     })
-  };
-    // console.log('req.url--------------------', req.url);
-  if (req.url.indexOf('.css') !== -1) {
+  }
+
+  else if (req.url.indexOf('.css') !== -1) {
+      console.log('inside-----------xxxxxxxxx')
+
     fs.readFile(archive.paths.css, function(error, css) {
       if (error) {
         throw error;
@@ -40,7 +54,29 @@ exports.sendResponse = function(req, res, statusCode){
       res.end(css); 
       exports.headers['Content-Type'] = "text/html";
     })
+  } 
+   else if (archive.paths.archivedSites + req.url) {
+    // console.log('inside-------------------------------- ')
+    var site = archive.paths.archivedSites + req.url;
+    // exports.headers['Content-Type'] = "text/plain";
+    fs.readFile(site, function(error, html) {
+      if (error) {
+        throw error;
+      }
+      res.writeHead(statusCode, exports.headers);
+      res.end(html);
+    })   
   }
+
+  // else {
+  //   statusCode = 404;
+  //   res.writeHead(statusCode, exports.headers);
+  //     // console.log("res----------------", res);
+  //   res.end();
+  // }
+
+
+
 }
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
